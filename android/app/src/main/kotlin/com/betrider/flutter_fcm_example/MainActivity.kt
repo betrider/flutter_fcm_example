e608fe50be83276b35c41df5b25a772f8d806caf
betrider <betrider@example.com>
2021-01-31 17:02:19 +0900

    FCM 수신 및 특정유저에게 메시지 보내기 구현 (IOS는 세팅안됨)

--- android/app/src/main/kotlin/com/betrider/flutter_fcm_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/betrider/flutter_fcm_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.betrider.flutter_fcm_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,165 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FCM'),
+        ),
+        body: MyHomePage(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            sendAndRetrieveMessage();
+          },
+          child: Icon(Icons.send),
+          backgroundColor: Colors.pink,
+        ),
+      ),
     );
+  }
+
+  Future<Map<String, dynamic>> sendAndRetrieveMessage() async{
+    final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+    var serverToken = 'AAAAPQqdszs:APA91bF3mtlbMoedwJBOX5jA9-_jhzTx0LwoR-azWE1wZcU7m6Ecn-PU2E91N0Xq_R1O23wrpS6VA4oi4XQBDwse8cdEKdiKRy-dWha_Y2_UVhVqopFVY9iEqYhxHq_m_7QX-znFT6pz';
+    await http.post(
+      'https://fcm.googleapis.com/fcm/send',
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=$serverToken',
+      },
+      body: jsonEncode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{
+            'body': 'this is a body',
+            'title': 'this is a title'
+          },
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done'
+          },
+          // 'to': await firebaseMessaging.getToken(),
+          'to': 'f0nbQHEWQmaMLxgjhgn-BJ:APA91bE0cC0QLsLxme6ich6Jb8DAY5pxiV_3RJ7n3zhobvnGDNpCkNKVusB6WgO8YeIKgKPhyxNCqdR6NzbUycozPUahciQPvjxgreM5OoCbLAWOu1FzoTGD7sICuCYe55LpEXlGvu4D',
+        },
+      ),
+    );
+
+    final Completer<Map<String, dynamic>> completer =
+    Completer<Map<String, dynamic>>();
+
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        completer.complete(message);
+      },
+    );
+
+    return completer.future;
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _MyHomePage();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePage extends State<MyHomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String token = '';
+  String test = 'Test';
+
+  @override
+  void initState() {
+    super.initState();
+    firebaseCloudMessaging_Listeners();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FutureBuilder(
+            future: _firebaseMessaging.getToken(),
+            builder: (context, snapshot){
+              token = snapshot.data;
+              return SelectableText(
+                'token : $token',
+              );
+            },
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'message : $test',
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void firebaseCloudMessaging_Listeners() {
+    if (Platform.isIOS) iOS_Permission();
+
+    _firebaseMessaging.getToken().then((token){
+      print('token:'+token);
+    });
+
+    _firebaseMessaging.configure(
+      //포그라운드
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+        setState(() {
+          test  = 'on message';
+        });
+      },
+      //백그라운드
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+        setState(() {
+          test  = 'on resume';
+        });
+      },
+      //꺼진경우
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+        setState(() {
+          test  = 'on launch';
+        });
+      },
+    );
+  }
+
+  void iOS_Permission() {
+    _firebaseMessaging.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true)
+    );
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings)
+    {
+      print("Settings registered: $settings");
+    });
   }
 }
